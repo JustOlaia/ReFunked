e0cc18bcc86c88085e1675aa1bae7e80cc074c5b
Olaia <104322336+JustOlaia@users.noreply.github.com>
2026-06-21 11:10:19 -0600

    Update FileUtil.hx

--- source/funkin/util/FileUtil.hx
+++ source/funkin/util/FileUtil.hx
@@ -299,19 +299,19 @@ class FileUtil
     }
 
     return false;
-    ‎#elseif android
-‎  haxe.ui.containers.dialogs.Dialogs.saveFile(function(button, result, path):Void
-‎  {
-‎    if (result)
-‎    {
-‎      if (onSave != null) { onSave(path ?? defaultFileName ?? ''); }
-‎    }
-‎    else
-‎    {
-‎      if (onCancel != null) { onCancel(); }
-‎    }
-‎  }, { name: defaultFileName, bytes: data });
-‎  return true;
+    #elseif android
+    haxe.ui.containers.dialogs.Dialogs.saveFile(function(button, result, path):Void
+    {
+      if (result)
+      {
+        if (onSave != null) { onSave(path ?? defaultFileName ?? ''); }
+      }
+      else
+      {
+        if (onCancel != null) { onCancel(); }
+      }
+    }, { name: defaultFileName, bytes: data });
+    return true;
     #else
     FileDialog.saveFile(Lib.current.stage.window, function(filepath:String, filter):Void
     {
